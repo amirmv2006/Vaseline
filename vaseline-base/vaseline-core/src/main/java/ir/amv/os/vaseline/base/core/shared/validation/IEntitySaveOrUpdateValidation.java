b94a25b39e73062c88a8c8f501cab9ef702a3f80
Amir MV <amirmv2006@gmail.com>
2017-04-11 16:21:42 +0800

    add saveOrUpdate validation group

---
+++ vaseline-base/vaseline-core/src/main/java/ir/amv/os/vaseline/base/core/shared/validation/IEntitySaveOrUpdateValidation.java
@@ -0,0 +1,7 @@
+package ir.amv.os.vaseline.base.core.shared.validation;
+
+/**
+ * Created by AMV on 4/11/2017.
+ */
+public interface IEntitySaveOrUpdateValidation {
+}
--- vaseline-base/vaseline-core/src/main/java/ir/amv/os/vaseline/base/core/shared/validation/IEntityUpdateValidation.java
+++ vaseline-base/vaseline-core/src/main/java/ir/amv/os/vaseline/base/core/shared/validation/IEntityUpdateValidation.java
@@ -3,5 +3,5 @@ package ir.amv.os.vaseline.base.core.shared.validation;
 /**
  * Created by AMV on 4/10/2017.
  */
-public interface IEntityUpdateValidation {
+public interface IEntityUpdateValidation extends IEntitySaveOrUpdateValidation {
 }
