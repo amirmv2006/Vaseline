ebaf6cfbee63267515b252586c8bedabd011dd2c
Amir <amirmv2006@gmail.com>
2018-02-10 22:59:30 +0800

    aggregating spring osgi versions

--- vaseline-ws-parent/vaseline-ws-osgi-parent/vaseline-ws-rest-secured-osgi/src/main/java/ir/amv/os/vaseline/ws/osgi/rest/secured/VaselineRestSecurityFilter.java
+++ vaseline-ws-parent/vaseline-ws-osgi-parent/vaseline-ws-rest-secured-osgi/src/main/java/ir/amv/os/vaseline/ws/osgi/rest/secured/VaselineRestSecurityFilter.java
@@ -12,8 +12,11 @@ import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * @author Amir
@@ -40,10 +43,16 @@ public class VaselineRestSecurityFilter implements ContainerRequestFilter {
     public void filter(final ContainerRequestContext requestContext) throws IOException {
         DelegatingFilterProxy delegatingFilterProxy = springApplicationContext.getBean(DelegatingFilterProxy.class, servletContext);
         delegatingFilterProxy.setServletContext(servletContext);
+        AtomicBoolean authenticationSuccess = new AtomicBoolean(false);
+        AtomicReference<Exception> cause = new AtomicReference<>(new Exception("Default Authentication Exception"));
         try {
-            delegatingFilterProxy.doFilter(servletRequest, servletResponse, (servletRequest, servletResponse) -> System.out.println("Done!"));
-        } catch (ServletException e) {
-            e.printStackTrace();
+            delegatingFilterProxy.doFilter(servletRequest, servletResponse, (servletRequest, servletResponse) ->
+                    authenticationSuccess.set(true));
+        } catch (Exception e) {
+            cause.set(e);
+        }
+        if (!authenticationSuccess.get()) {
+            requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).build());
         }
     }
 
