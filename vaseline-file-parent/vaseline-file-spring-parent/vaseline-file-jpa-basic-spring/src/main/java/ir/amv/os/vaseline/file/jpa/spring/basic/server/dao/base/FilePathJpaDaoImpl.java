7444dac47b83188411a3a6b77615338018008783
Amir <AmirMV2006@gmail.com>
2020-01-01 21:00:55 +0100

    skip security modules for now...

--- vaseline-file-parent/vaseline-file-spring-parent/vaseline-file-jpa-basic-spring/src/main/java/ir/amv/os/vaseline/file/jpa/spring/basic/server/dao/base/FilePathJpaDaoImpl.java
+++
@@ -1,6 +0,0 @@
-package ir.amv.os.vaseline.file.jpa.spring.basic.server.dao.base;
-
-/**
- * @author Amir
- */
-public class FilePathJpaDaoImpl {}
--- vaseline-ws-parent/vaseline-ws-rest/src/main/java/ir/amv/os/vaseline/ws/rest/server/base/parent/impl/BaseRestServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package ir.amv.os.vaseline.ws.rest.server.base.parent.impl;
-
-import ir.amv.os.vaseline.ws.rest.server.base.parent.IBaseRestService;
-
-/**
- * Created by AMV on 2/13/2016.
- */
-public class BaseRestServiceImpl implements IBaseRestService {
-}
